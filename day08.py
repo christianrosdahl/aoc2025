387c05134383ae946fa90fab0a27df71a3d29fae
Christian Rosdahl <christian.rosdahl@gmail.com>
2025-12-08 20:34:05 +0100

    Add improved (much faster) solutions for day 8

--- day08.py
+++ day08.py
@@ -4,6 +4,7 @@ import math
 from common import get_input
 
 coord = tuple[int, int, int]
+box_pair = tuple[coord, coord]
 
 
 # Part 1
@@ -11,12 +12,8 @@ def main1() -> str | int | None:
     input_data = get_input(8)
     num_connections = 1000
     boxes = parse_input(input_data)
-    connections = {box: set() for box in boxes}
-    distances = {(box1, box2): dist(box1, box2) for box1 in boxes for box2 in boxes}
-    for i in range(num_connections):
-        print(f"Connection {i+1} of {num_connections}")
-        box1, box2 = closest_non_connected(connections, distances)
-        connect(connections, box1, box2)
+    distances = get_sorted_distances(boxes)
+    connections, _ = get_connections(num_connections, boxes, distances)
     circuits = get_circuits(connections)
     circuit_sizes = sorted([len(circuit) for circuit in circuits], reverse=True)
     return circuit_sizes[0] * circuit_sizes[1] * circuit_sizes[2]
@@ -26,37 +23,34 @@ def parse_input(input_data: list[str]) -> list[coord]:
     return [tuple(int(i) for i in line.split(",")) for line in input_data]
 
 
+def get_sorted_distances(boxes: list[coord]) -> dict[box_pair, int]:
+    box_pairs = {
+        (b1, b2) for i, b1 in enumerate(boxes) for j, b2 in enumerate(boxes) if i < j
+    }
+    distances = {(box1, box2): dist(box1, box2) for (box1, box2) in box_pairs}
+    return dict(sorted(distances.items(), key=lambda x: x[1]))
+
+
 def dist(box1: coord, box2: coord) -> float:
     diff = tuple(box2[i] - box1[i] for i in range(3))
     return math.sqrt(sum(diff[i] ** 2 for i in range(3)))
 
 
+def get_connections(
+    num_connections: int, boxes: list[coord], distances: dict[box_pair, int]
+) -> tuple[dict[coord, set[coord]], box_pair]:
+    connections = {box: set() for box in boxes}
+    for i, (box1, box2) in enumerate(distances.keys()):
+        if i == num_connections:
+            break
+        connect(connections, box1, box2)
+    last_connected = (box1, box2)
+    return connections, last_connected
+
+
 def connect(connections: dict[coord, set[coord]], box1: coord, box2: coord):
     connections[box1].add(box2)
     connections[box2].add(box1)
-
-
-def are_connected(
-    connections: dict[coord, set[coord]], box1: coord, box2: coord
-) -> bool:
-    if box1 == box2:
-        return True
-    return box2 in connections[box1]
-
-
-def closest_non_connected(
-    connections: dict[coord, set[coord]], distances: dict[tuple[coord, coord], float]
-) -> tuple[coord, coord] | None:
-    boxes = list(connections.keys())
-    min_dist = math.inf
-    result = None
-    for box1 in boxes:
-        for box2 in boxes:
-            connected = are_connected(connections, box1, box2)
-            if distances[(box1, box2)] < min_dist and not connected:
-                min_dist = dist(box1, box2)
-                result = (box1, box2)
-    return result
 
 
 def get_circuits(connections: dict[coord, set[coord]]) -> list[set[coord]]:
@@ -88,20 +82,55 @@ def get_connected(
 def main2() -> str | int | None:
     input_data = get_input(8)
     boxes = parse_input(input_data)
-    connections = {box: set() for box in boxes}
-    distances = {(box1, box2): dist(box1, box2) for box1 in boxes for box2 in boxes}
-    num_connections = 0
-    while True:
-        box1, box2 = closest_non_connected(connections, distances)
-        connect(connections, box1, box2)
-        num_connections += 1
-        circuits = get_circuits(connections)
-        print(f"Num connections: {num_connections}, Num circuits: {len(circuits)}")
-        if len(circuits) == 1:
-            last_connection = (box1, box2)
-            break
+    distances = get_sorted_distances(boxes)
+
+    min_num_connections = 1
+    max_num_connections = get_max_num_connections(boxes, distances)
+    num_connections = find_num_connections(
+        min_num_connections, max_num_connections, boxes, distances
+    )
+    _, last_connection = get_connections(num_connections, boxes, distances)
     box1, box2 = last_connection
     return box1[0] * box2[0]
+
+
+def get_num_circuits(
+    num_connections: int,
+    boxes: list[coord],
+    distances: dict[box_pair, int],
+) -> int:
+    connections, _ = get_connections(num_connections, boxes, distances)
+    circuits = get_circuits(connections)
+    return len(circuits)
+
+
+def get_max_num_connections(
+    boxes: list[coord],
+    distances: dict[box_pair, int],
+) -> int:
+    num_circuits = None
+    max_num_connections = 1
+    while not num_circuits or num_circuits > 1:
+        max_num_connections = max_num_connections * 2
+        num_circuits = get_num_circuits(max_num_connections, boxes, distances)
+    return max_num_connections
+
+
+def find_num_connections(
+    min_num: int,
+    max_num: int,
+    boxes: list[coord],
+    distances: dict[box_pair, int],
+) -> int:
+    if min_num == max_num or min_num == max_num - 1:
+        return min_num
+    middle_num = math.ceil((max_num + min_num) / 2)
+    middle_num_circuits = get_num_circuits(middle_num, boxes, distances)
+    if middle_num_circuits > 1:
+        min_num = middle_num
+    else:
+        max_num = middle_num
+    return find_num_connections(min_num, max_num, boxes, distances)
 
 
 if __name__ == "__main__":
